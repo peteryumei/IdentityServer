e99faefdbbd2ee0312125d6b10e73c655555ce0c
Peter Mei <petermei@yahoo.com>
2018-02-05 21:32:54 -0800

    Add UI

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -15,6 +15,8 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddIdentityServer().AddDeveloperSigningCredential()
                 .AddInMemoryClients(Config.GetClients())
                 .AddInMemoryApiResources(Config.GetApiResources())
@@ -30,6 +32,8 @@ namespace IdentityServer
             }
 
             app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
             //app.Run(async (context) =>
             //{
