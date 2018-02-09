bcd26a89c702c75a8b1e3e5101c3e96caa0afae4
Peter Mei <petermei@yahoo.com>
2018-02-08 20:53:25 -0800

    Update

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 
@@ -25,9 +27,33 @@ namespace IdentityServer
                 AllowedScopes = {"api1"}
             });
 
+            //Client for MVC
+            clients.Add(new Client()
+            {
+                ClientId = "mvc",
+                ClientName = "MVC",
+                AllowedGrantTypes = GrantTypes.Implicit,
+
+                //where to redirect to after login
+                RedirectUris = { "http://localhost:13998/signin-oidc" },
+
+                //where to redirect to after Logout
+                PostLogoutRedirectUris = { "http://localhost:13998/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "roles"
+                },
+                AlwaysIncludeUserClaimsInIdToken = true   //It should be true to send Claims with the token
+
+            });
+
             return clients;
         }
 
+        //Defining the InMemory API's
         public static IEnumerable<ApiResource> GetApiResources()
         {
             List<ApiResource> apiResources = new List<ApiResource>();
@@ -43,11 +69,42 @@ namespace IdentityServer
             testUsers.Add(new TestUser()
             {
                 SubjectId= "1",
-                Username = "peter",
-                Password = "password"
+                Username = "admin",
+                Password = "password",
+
+                Claims = new List<Claim>
+                {
+                    new Claim("role", "admin")
+                }
             });
 
+            testUsers.Add(new TestUser()
+            {
+                SubjectId = "1",
+                Username = "editor",
+                Password = "password",
+
+                Claims = new List<Claim>
+                {
+                    new Claim("role", "editor")
+                }
+            });
+
+
+
             return testUsers;
+        }
+
+        //Support for OpenId connectivity scopes
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            List<IdentityResource> resources = new List<IdentityResource>();
+
+            resources.Add(new IdentityResources.OpenId());
+            resources.Add(new IdentityResources.Profile());
+            resources.Add(new IdentityResource("roles", new[] { "role" }));
+
+            return resources;
         }
     }
 
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -20,7 +20,8 @@ namespace IdentityServer
             services.AddIdentityServer().AddDeveloperSigningCredential()
                 .AddInMemoryClients(Config.GetClients())
                 .AddInMemoryApiResources(Config.GetApiResources())
-                .AddTestUsers(Config.GetUsers());
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryIdentityResources(Config.GetIdentityResources());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
